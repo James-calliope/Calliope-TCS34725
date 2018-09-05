b2340260bae5d0ebbad34b7edc8b2f7c2539cf1e
ThinkManhattan <598612505@qq.com>
2018-09-05 23:25:02 +0800

    second commit

--- test.ts
+++ test.ts
@@ -1,16 +1,24 @@
 // tests go here; this will not be compiled when this package is used as a library
-let r =0;
-let g =0;
-let b =0;
-let c =0;
-let colorTemp:number =0;
-let lux =0;
-let err_code = TCS34725_I2C.init_TCS34725();
+let lux: number = 0;
+let colorTemp: number = 0;
+let r: number;
+let g: number;
+let b: number;
+let c: number;
+let err_code: number;
+err_code =  TCS34725_I2C.init_TCS34725();
+serial.writeValue("err_code", err_code);
 basic.forever(() => {
     TCS34725_I2C.getRawData();
+    r=TCS34725_I2C.get_r_data();
+    g=TCS34725_I2C.get_g_data();
+    b=TCS34725_I2C.get_b_data();
+    serial.writeValue("r", r);
+    serial.writeValue("g", g);
+    serial.writeValue("b", b);
     colorTemp = TCS34725_I2C.calculateColorTemperature();
-    serial.writeValue("colorTemp",colorTemp);
+    serial.writeValue("colorTemp", colorTemp);
     lux = TCS34725_I2C.calculateLux();
-    serial.writeValue("lux",lux)
-    basic.pause(1000)
+    serial.writeValue("lux", lux);
+    basic.pause(2000);
 })
